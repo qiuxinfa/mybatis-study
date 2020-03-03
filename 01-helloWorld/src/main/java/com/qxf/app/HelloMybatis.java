40ee46987e68e6bfc4cf5c2e0fe70cf7833eaa8f
xinfa Qiu <1816522516@qq.com>
2020-03-03 22:25:24 +0800

    添加相关说明，使用列别名

--- 01-helloWorld/src/main/java/com/qxf/app/HelloMybatis.java
+++ 01-helloWorld/src/main/java/com/qxf/app/HelloMybatis.java
@@ -16,11 +16,17 @@ import java.io.InputStream;
  */
 public class HelloMybatis {
     public static void main(String[] args) throws IOException{
+        //读取配置信息
         InputStream  inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+        //根据配置信息，创建SqlSession工厂
         SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(inputStream);
+        //SqlSession工厂创建SqlSession
         SqlSession sqlSession = factory.openSession();
-        User user = (User)sqlSession.selectOne("hello.mybatis.test.getUserById", 123);
+        //SqlSession进行数据库的CRUD操作
+        //这里的selectOne是查询一个的意思，返回0个或1个对象
+        User user = (User)sqlSession.selectOne("hello.mybatis.test.getUserById", "123");
         System.out.println(user);
+        //关闭会话
         sqlSession.close();
     }
 }
